59245ed2effbf92aff93d4476025277ea6c7bbe3
ceilingfan-404 <145jwu@gmail.com>
2026-07-05 15:08:47 -0500

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Jinglian" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print(f"{name}, Welcome to the CSS course!")
 if __name__ == "__main__":
     main()
